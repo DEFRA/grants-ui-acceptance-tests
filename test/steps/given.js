9c893558991834113f6a37fbeec94ce235065b87
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-10-04 20:14:27 +0100

    Automate Defra ID sign-out scenario

--- test/steps/given.js
+++ test/steps/given.js
@@ -1,5 +1,6 @@
 import { Given } from '@wdio/cucumber-framework'
 import { pollForSuccess } from '../services/polling'
+import DefraAccountBar from '../page-objects/defra-account-bar'
 
 Given('(the user )navigates to {string}', async (page) => {
   await browser.url(page)
@@ -12,9 +13,15 @@ Given('(the user )completes any login process as CRN {string}', async (crn) => {
     await $(`//input[@id='crn']`).setValue(crn)
     await $(`//input[@id='password']`).setValue(process.env.DEFRA_ID_USER_PASSWORD)
     await $(`//button[@type='submit']`).click()
-    // allow extra time for Defra ID login to succeed
+    // allow extra time for Defra ID sign in to succeed
     await expect(browser).not.toHaveUrl(expect.stringContaining('b2clogin.com'), { wait: 20000 })
   }
+})
+
+Given('(the user )signs out of Defra ID', async () => {
+  await DefraAccountBar.signOut()
+    // allow extra time for Defra ID sign out to succeed
+  await expect($(`//h1[contains(text(),'Sign into your Defra account')]`)).toBeDisplayed({ wait: 20000 })
 })
 
 Given('(the user )selects SBI {string}', async (sbi) => {
