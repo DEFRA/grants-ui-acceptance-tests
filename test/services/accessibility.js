9a63f88b6d4fe0e4a4362bf889793909e8b45e14
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-12-10 21:42:29 +0000

    Refactor accessibility testing #3

--- test/services/accessibility.js
+++ test/services/accessibility.js
@@ -1,13 +1,14 @@
 import AxeBuilder from '@axe-core/webdriverio'
 import allureReporter from '@wdio/allure-reporter'
 
+const IGNORED_VIOLATIONS = ['Ensure all page content is contained by landmarks']
+
 export async function analyzeAccessibility() {
   const results = await new AxeBuilder({ client: browser }).withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice']).analyze()
 
-  const wcagViolations = results.violations.filter((violation) => violation.tags.some((tag) => tag.startsWith('wcag')))
-  expect(wcagViolations).toEqual([])
+  allureReporter.addAttachment(`WCAG Analysis`, JSON.stringify(results.violations, null, 2), 'application/json')
 
-  if (results.violations.length > 0) {
-    allureReporter.addAttachment(`WCAG Best Practice Violations`, JSON.stringify(results.violations, null, 2), 'application/json')
-  }
+  const unexpectedViolations = results.violations.filter((violation) => !IGNORED_VIOLATIONS.includes(violation.description))
+
+  expect(unexpectedViolations).toEqual([])
 }
