0c515e9900fb08234db89c1e87445f4f99f4759e
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-12-10 20:52:54 +0000

    Refactor accessibility testing #2

--- test/services/accessibility.js
+++ test/services/accessibility.js
@@ -4,11 +4,10 @@ import allureReporter from '@wdio/allure-reporter'
 export async function analyzeAccessibility() {
   const results = await new AxeBuilder({ client: browser }).withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice']).analyze()
 
+  const wcagViolations = results.violations.filter((violation) => violation.tags.some((tag) => tag.startsWith('wcag')))
+  expect(wcagViolations).toEqual([])
+
   if (results.violations.length > 0) {
     allureReporter.addAttachment(`WCAG Best Practice Violations`, JSON.stringify(results.violations, null, 2), 'application/json')
   }
-
-  const wcagViolations = results.violations.filter((violation) => violation.tags.some((tag) => tag.startsWith('wcag')))
-
-  expect(wcagViolations).toEqual([])
 }
