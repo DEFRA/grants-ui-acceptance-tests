8e5fc1da6c2a02224c9b851360b22608029a2e35
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-12-11 05:08:19 +0000

    Refactor accessibility checking further

--- test/services/accessibility.js
+++ test/services/accessibility.js
@@ -1,14 +1,34 @@
 import AxeBuilder from '@axe-core/webdriverio'
 import allureReporter from '@wdio/allure-reporter'
 
-const IGNORED_VIOLATIONS = ['Ensure all page content is contained by landmarks']
+const IGNORED_VIOLATIONS = [
+  {
+    description: 'Ensure all page content is contained by landmarks',
+    ignoredTargets: ['.govuk-back-link', '.govuk-skip-link', '.govuk-width-container:nth-child(7)', '.govuk-width-container:nth-child(8)']
+  }
+]
 
 export async function analyzeAccessibility() {
   const results = await new AxeBuilder({ client: browser }).withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'best-practice']).analyze()
 
   allureReporter.addAttachment(`WCAG Analysis`, JSON.stringify(results.violations, null, 2), 'application/json')
 
-  const unexpectedViolations = results.violations.filter((violation) => !IGNORED_VIOLATIONS.includes(violation.description))
+  const unexpectedViolations = extractUnexpectedViolations(results.violations)
 
   expect(unexpectedViolations).toEqual([])
+}
+
+function extractUnexpectedViolations(violations) {
+  return violations
+    .map((violation) => ({
+      ...violation,
+      nodes: violation.nodes.filter((node) => !isIgnoredNode(node, violation))
+    }))
+    .filter((violation) => violation.nodes.length > 0)
+}
+
+function isIgnoredNode(node, violation) {
+  return IGNORED_VIOLATIONS.some((ignored) => {
+    return ignored.description === violation.description && ignored.ignoredTargets.some((ignoredTarget) => node.target[0] === ignoredTarget)
+  })
 }
